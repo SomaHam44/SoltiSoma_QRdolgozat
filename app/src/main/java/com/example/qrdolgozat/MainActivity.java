b58dbf001783a7a8d2f0bed7dcc6596f618d785e
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-01-26 19:44:05 +0100

    Implementation, Szkennelés

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.qrdolgozat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonScan, buttonKiir;
@@ -15,6 +23,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setPrompt("QR Code szkennelés");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.initiateScan();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult eredmeny = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (eredmeny != null) {
+            if (eredmeny.getContents() == null) {
+                Toast.makeText(this, "Kilépés a szkennelésből", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                textViewSzoveg.setText(eredmeny.getContents());
+                try {
+                    Uri url = Uri.parse(eredmeny.getContents());
+                    Intent intent = new Intent (Intent.ACTION_VIEW, url);
+                    startActivity(intent);
+                }
+                 catch (Exception e) {
+                    Log.d("URL ERROR", e.toString());
+                 }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void init() {
