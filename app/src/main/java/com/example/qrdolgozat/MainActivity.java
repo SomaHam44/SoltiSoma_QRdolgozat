e83fc1be50a25e9384f808a633a2fd83ee424063
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-01-26 20:03:09 +0100

    Módosítás, buttonKiir onClickListener előkészítése

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -35,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        buttonKiir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
