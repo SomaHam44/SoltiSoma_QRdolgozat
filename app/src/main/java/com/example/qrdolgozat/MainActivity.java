dada19fb96c4b85afd262d7710a3dc6c1aa0dc5a
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-01-26 19:31:19 +0100

    Privát adattagok, init

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.qrdolgozat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonScan, buttonKiir;
+    private TextView textViewSzoveg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        buttonScan = findViewById(R.id.buttonScan);
+        buttonKiir = findViewById(R.id.buttonKiir);
+        textViewSzoveg = findViewById(R.id.textSzoveg);
     }
 }
